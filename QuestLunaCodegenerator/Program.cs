190de33f9092cda87cadbbb43a114b3e78e38709
Millzy <millzyyt08@gmail.com>
2022-07-14 19:54:52 +0100

    main

---
+++ QuestLunaCodegenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LunaCodegenerator.Quest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Drag and drop the codegen JSON output file into this window and press Enter.");
+                string jsonPath = Console.ReadLine();
+
+                Console.WriteLine("Enter the output path for the project and press Enter.");
+                string outputPath = Console.ReadLine();
+
+#if DEBUG
+                Console.WriteLine($"jsonPath={jsonPath}\noutputPath={outputPath}");
+#endif
+                var config = new GenerationConfig(new IO(), jsonPath, outputPath);
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        private void Generate(GenerationConfig config)
+        {
+
+        }
+    }
+}
